affd8e7abcae78c0f88fe820cb39df8f604dbbb4
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-10-31 22:02:05 +0100

    cabios en variables en funciones y agregamos funcion de login

--- func.php
+++ func.php
@@ -1,10 +1,36 @@
 <?php
+
+
+// Funcion para iniciar sesión en la cuenta 
+function login($email, $passw) {
+    require_once "conection.php";
+
+    $bd = new PDO("mysql:dbname=".$bd_config["bd_name"].";host=".$bd_config["ip"], 
+        $bd_config["user"],
+        $bd_config["password"]);
+    $ins = "select * from users where email = '$email' and passw = '$passw'";
+    $resul = $bd->query($ins);
+    foreach ($resul as $row) {
+        // alamacenar el rol en la sesión
+        $_SESSION['rol'] = $row['rol'];
+        return TRUE;
+    }   
+    if($resul->rowCount() === 1){        
+        return $resul->fetch();        
+    }else{
+        return FALSE;
+    }
+}
+
+
+
+// Función que crea un ticket en la base de datos
 function create_ticket($subject, $description, $priority, $email) {
     require_once "conection.php";
 
-    $bd = new PDO("mysql:dbname=".$bd_config["nombrebd"].";host=".$bd_config["ip"], 
-                    $bd_config["usuario"],
-                    $bd_config["clave"]);
+    $bd = new PDO("mysql:dbname=".$bd_config["bd_name"].";host=".$bd_config["ip"], 
+        $bd_config["user"],
+        $bd_config["password"]);
     $ins = "insert into tickets values (subject, description, priority, user) values ('$subject', '$description', '$priority', '$email')";
     $resul = $bd->query($ins);    
     if($resul->rowCount() === 1){        
