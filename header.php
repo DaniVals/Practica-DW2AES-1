5431c1f46ab6e1f92b2463cc8cd42397900a3a6d
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-10-31 12:32:09 +0100

    creacion de la cabezera

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+ Usuario: <?= $_SESSION['user']['email']?>
+ <a href="ticket_list.php">Mis tickets</a>
+ <a href="create_ticket.php">Crear un ticket</a> 
+ <a href="logout.php">Cerrar sesión</a>
+</header>
+<hr>
