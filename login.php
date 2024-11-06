26f6d94122cc1f31b3c99b1c6d5be5f930333fbf
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-11-06 08:58:28 +0100

    fix: bug on EOF

--- login.php
+++ login.php
@@ -1,16 +1,25 @@
+<?php
+    // Iniciar sesion
+    session_start();
+    // Comprobar si el usuario ya esta logueado
+    if (isset($_SESSION["email"])) {
+        header("Location: ticket_list.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 
     <!-- Imprimir formulario si entramos por GET -->
-    <?php if ($_SERVER["REQUEST_METHOD"] == "GET") { ?>
-
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "GET") { 
+    ?>
     <div id="div-login">
         <form method="POST">
             Usuario: <input type="text" name="email"><br>
@@ -26,12 +35,11 @@
         // TODO seguir cuando tengamos BBDD
         // Incluir la funcion para iniciar sesion
         if (login($_POST["email"], $_POST["passw"])) {
-            // Revisar el rol del usuario (No lo pongo porque no se si tenemos que hacer algo con el rol)
             header("Location: ticket_list.php");
         } else {
             echo "Error al iniciar sesión";
-        } 
+        }
+    } 
     ?>
-
 </body>
 </html>
