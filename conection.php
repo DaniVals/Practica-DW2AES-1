4beddfddea55ba59aaa88a0b44e552a6499c727b
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-11-07 14:09:53 +0100

    feat: security patch

--- conection.php
+++ conection.php
@@ -1,4 +1,6 @@
 <?php
+    // Que no se pueda acceder a este archivo directamente
+    header("Location: index.php");
     // Configuración de la base de datos en un array que se incluirá en las funciones de bd.php
     $bd_config = ["ip" => "127.0.0.1",
                  "bd_name" => "projectdwes_1term",
