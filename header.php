00ccde6ee8692ba1f621680979e611b097a8d6b2
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-11-12 13:57:08 +0100

    feat: add Profile

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <header>
     <link rel="stylesheet" href="css/header.css">
     Usuario: <?= $_SESSION['email']?>
+    <a href="profile.php">Mi perfil</a>
     <a href="ticket_list.php">Mis tickets</a>
     <?php if ($_SESSION['rol'] != 1) {
     // poner el 'crear ticket' si NO es tecnico
