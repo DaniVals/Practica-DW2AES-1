bc864253127b860621990b7833590bedc681175a
Iván Arroyo González <ivanag2005ag@gmail.com>
2024-11-14 19:54:27 +0100

    fix: enormus fucking bug

--- confirmation_panel.php
+++ confirmation_panel.php
@@ -1,14 +1,25 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["email"])) {
-    $email = $_POST["email"];
+session_start();
+if (!isset($_SESSION["email"])) {
+    header("Location: login.php");
+}
+$email = $_SESSION["email"];
 ?>
-<p>¿Estás seguro de que quieres cerrar tu cuenta?</p>
-<form action="close_account.php" method="POST"> 
+<p>¿Estás seguro de que quieres cerrar la cuenta <?= $email?>?</p>
+<form method="POST"> 
     <input type="hidden" name="email" value="<?= $email ?>">
-    <input type="submit" value="Sí">
+    <input type="submit" value="Sí" name="Si">
 </form>
 <form action="profile.php" method="GET">
     <input type="submit" value="No">
 </form>
 <?php
-}
+    include "func.php";
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["Si"])) {
+        close_account($email);
+        header("Location: login.php");
+    }
+    if ($_SERVER["REQUEST_METHOD"] === "GET") {
+        header("Location: profile.php");
+    }
+?>
