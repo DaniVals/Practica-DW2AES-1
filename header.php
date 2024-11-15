25e866ea13e22d8530fe9d8561e78619874dd6fd
navarra3103 <nvr312003@gmail.com>
2024-11-15 19:37:43 +0100

    css: login & header

--- header.php
+++ header.php
@@ -1,18 +1,27 @@
 <header>
+    
+    <div id="menu">
+
+        <a href="profile.php">Mi perfil</a>
+        
+        <?php if ($_SESSION['rol'] == 1) {
+            echo "<a href='ticket_list.php'>Lista de tickets</a>";
+        } else {
+            echo "<a href='ticket_list.php'>Mis tickets</a>";
+        } ?>
+        
+        
+        <?php if ($_SESSION['rol'] != 1) {
+            // poner el 'crear ticket' si NO es tecnico
+            echo "<a href='create_ticket.php'>Crear un ticket</a>"; 
+        } ?>
+        <a href="logout.php">Cerrar sesión</a>
+        
+    </div>
+
     <link rel="stylesheet" href="css/header.css">
-    Usuario: <?= $_SESSION['email']?>
-    <a href="profile.php">Mi perfil</a>
-    <a href="ticket_list.php">
-    <?php if ($_SESSION['rol'] == 1) {
-        echo "Lista de tickets";
-    } else {
-        echo "Mis tickets";
-    } ?>
-    </a>
-    <?php if ($_SESSION['rol'] != 1) {
-    // poner el 'crear ticket' si NO es tecnico
-    echo "<a href='create_ticket.php'>Crear un ticket</a>"; 
-    } ?>
-    <a href="logout.php">Cerrar sesión</a>
+    <p> Usuario: <?= $_SESSION['email']?> </p>
+
     <hr>
+
 </header>
