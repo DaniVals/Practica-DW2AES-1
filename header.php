c6556a2037db59b7babe6594d986ec8fe689c55b
Daniel Vals <daniel.vals.simon@gmail.com>
2024-11-15 10:56:40 +0100

    cambiar PORFIN los tickets

--- header.php
+++ header.php
@@ -2,10 +2,16 @@
     <link rel="stylesheet" href="css/header.css">
     Usuario: <?= $_SESSION['email']?>
     <a href="profile.php">Mi perfil</a>
-    <a href="ticket_list.php">Mis tickets</a>
+    <a href="ticket_list.php">
+    <?php if ($_SESSION['rol'] == 1) {
+        echo "Lista de tickets";
+    } else {
+        echo "Mis tickets";
+    } ?>
+    </a>
     <?php if ($_SESSION['rol'] != 1) {
     // poner el 'crear ticket' si NO es tecnico
-    echo '<a href="create_ticket.php">Crear un ticket</a>'; 
+    echo "<a href='create_ticket.php'>Crear un ticket</a>"; 
     } ?>
     <a href="logout.php">Cerrar sesión</a>
     <hr>
